d24795f1bf6707e0c672e485c6cb3be4d38ed75f
nawfalkaysan <nawfalkasyanre@gmail.com>
2025-05-01 07:55:00 +0700

    update agar ngrok agar flask bisa diakses publik

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ placeholder = st.empty()
 # Jalankan loop update otomatis
 while True:
     try:
-        response = requests.get("http://localhost:5000/api/data")
+        response = requests.get("https://0989-103-136-58-244.ngrok-free.app/api/data")
         data = response.json()
 
         with placeholder.container():
